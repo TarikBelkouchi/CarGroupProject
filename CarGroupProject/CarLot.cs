de308b17c5607e00bde13d33715a5de6ccb10e64
Aaliyah Kayum <kayumaaliyah@gmail.com>
2022-02-15 16:42:07 -0500

    updated carlotlist

---
+++ CarGroupProject/CarLot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class CarLot : Cars
+{
+    public List<Cars> CarLotList { get; set; }
+        
+    public CarLot()
+
+        public void removeCarLotList(Cars x)
+    {
+        carLotList.Remove(x);
+    }
+
+
+    public void AddCarLotList(Cars x)
+    {
+        CarLotList.add(x)
+            }
+    public void PrintCarLotList()
+    {
+        foreach (Cars x in CarLotList)
+        {
+            Console.WriteLine(x);
+        }
+    }
+}
+
+
+
--- CarGroupProject/Program.cs
+++ CarGroupProject/Program.cs
@@ -4,3 +4,7 @@ Cars Mustang = new Cars("Ford", "Mustang", 1969, 50000);
 Console.WriteLine(Mustang.ToString());
 UsedCars Golf = new UsedCars("VW", "Golf", 2002, 10000, 78000);
 Console.WriteLine(Golf.ToString());
+
+List<Cars> cars = new List<Cars>();
+cars.Add(Mustang);
+cars.Add(Golf);
